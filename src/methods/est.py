3203a048f5467208549ea691dfff3288ace834f0
Leonardo Assis <leosassis@gmail.com>
2025-04-15 09:12:10 -0400

    est code refactoring

--- src/methods/est.py
+++ src/methods/est.py
@@ -1,60 +1,83 @@
 from pyomo.environ import *
 from itertools import product
+from numpy import ceil
 
-
-INITIAL_SHIFT = 1
-SHIFT_TO_END_RUN = 1
+INITIAL_SHIFT = 1  # Initial shift of a consuming task in case the methods calculates that it should start before the start of the producing task
+SHIFT_TO_END_RUN = 1  # Shift to reach the end of a consuming task run
 
 
 def materials_to_be_explored(STN: dict) -> set:
-    STATES = STN['STATES']
-    S_SubSet_Materials = S_SubSet_Materials = {k: v for k, v in STATES.items() if v['isIntermed']}
-    S_Material_To_Be_Explored = sorted(S_SubSet_Materials.keys(), key=lambda k: S_SubSet_Materials[k]['order'])
+    """ 
+    Return an ordered set of materials to be explored.
+    """
     
-    return S_Material_To_Be_Explored
+    STATES = STN['STATES']
+    S_SubSet_Materials = set({k: v for k, v in STATES.items() if v['isIntermed']})
+        
+    return sorted(S_SubSet_Materials.keys(), key=lambda k: S_SubSet_Materials[k]['order'])
 
 
 def get_production_relationship(model: ConcreteModel, i_producing: Any, j_producing: Any, ii_consuming: Any, jj_consuming: Any) -> int:
+    """ 
+    Computes the production relationship between a consuming task ii_consuming and a producing task i_producing.
+    """
     
-    return (model.P_Tau_Min[ii_consuming,jj_consuming]*model.P_Beta_Min[ii_consuming,jj_consuming]) / (model.P_Tau_Max[i_producing, j_producing] * model.P_Beta_Max[i_producing, j_producing])
+    numerator = model.P_Tau_Min[ii_consuming,jj_consuming]*model.P_Beta_Min[ii_consuming,jj_consuming]
+    denominator = model.P_Tau_Max[i_producing, j_producing] * model.P_Beta_Max[i_producing, j_producing]
+    
+    return numerator/denominator
 
 
 def get_number_periods(model: ConcreteModel, i_producing: Any, j_producing: Any, ii_consuming: Any, jj_consuming: Any, production_relationship: dict) -> int:
+    """ 
+    Computes the number of periods necessary for task i_producing to produce enough material to start a run of task ii_consuming.
+    """
     
-    return (ceil(production_relationship[i_producing, j_producing, ii_consuming, jj_consuming] * model.P_Tau_Max[i_producing,j_producing]) 
-          + ceil(production_relationship[i_producing, j_producing, ii_consuming, jj_consuming] * model.P_Tau_End_Task[i_producing]) 
-          - model.P_Tau_End_Task[i_producing])
+    rel = production_relationship[i_producing, j_producing, ii_consuming, jj_consuming]
+    tau_max = model.P_Tau_Max[i_producing,j_producing]
+    tau_end = model.P_Tau_End_Task[i_producing]
+    
+    return ceil(rel * tau_max) + ceil(rel * tau_end) - tau_end
 
 
 def get_est(model: ConcreteModel, i_producing: Any, j_producing: Any, ii_consuming: Any, jj_consuming: Any, number_periods: dict, est_task: dict) -> int:
-
-    return (max(est_task[j_producing, i_producing] + INITIAL_SHIFT, 
-                est_task[j_producing, i_producing] + number_periods[i_producing, j_producing, ii_consuming, jj_consuming] + SHIFT_TO_END_RUN - model.P_Tau_Min[ii_consuming, jj_consuming]))
+    """ 
+    Computes the est of consuming task ii_consuming based on the est of the producing task i_producing, the production relationship and the number of periods.
+    """
+    
+    est_prod_task = est_task[j_producing, i_producing]
+    num_periods = number_periods[i_producing, j_producing, ii_consuming, jj_consuming]
+    tau_min_consuming_task = model.P_Tau_Min[ii_consuming, jj_consuming]
+    
+    return max(est_prod_task + INITIAL_SHIFT, est_prod_task + num_periods + SHIFT_TO_END_RUN - tau_min_consuming_task)
 
 
 def compute_est_cuts(model: ConcreteModel, STN: dict) -> None:
-    UNIT_TASKS = STN['UNIT_TASKS']
-    S_Material_To_Be_Explored = materials_to_be_explored(STN)
+    """ 
+    Computes the est for all tasks in the STN network.
+    Updates the STN dictionary with 'EST_ST', a mapping of (j,i) -> est value.
+    """
+    
+    unit_task = STN['UNIT_TASKS']
+    materials_to_explore = materials_to_be_explored(STN)
     
     production_relationship = {}
     number_periods = {}
-    est_task = {(j,i): 0 for (j,i) in UNIT_TASKS} 
+    est_task = {(j,i): 0 for (j,i) in unit_task} 
     
-    while S_Material_To_Be_Explored:
+    while materials_to_explore:
         
-        k = S_Material_To_Be_Explored.pop(0)
+        k = materials_to_explore.pop(0)
                
         for i_producing, ii_consuming in product(model.S_I_Producing_K[k], model.S_I_Consuming_K[k]):
                 
             j_producing = next(iter(model.S_J_Executing_I[i_producing]))
             jj_consuming = next(iter(model.S_J_Executing_I[ii_consuming]))
+            
+            key = (i_producing, j_producing, ii_consuming, jj_consuming)
                 
-            production_relationship[i_producing, j_producing, ii_consuming, jj_consuming] = get_production_relationship(model, i_producing, j_producing, ii_consuming, jj_consuming)
-            number_periods[i_producing, j_producing, ii_consuming, jj_consuming] = get_number_periods(model, i_producing, j_producing, ii_consuming, jj_consuming, production_relationship) 
-            est_task[jj_consuming, ii_consuming] = get_est(model, i_producing, j_producing, ii_consuming, jj_consuming, number_periods, est_task) 
+            production_relationship[key] = get_production_relationship(model, *key)
+            number_periods[key] = get_number_periods(model, *key, production_relationship) 
+            est_task[jj_consuming, ii_consuming] = get_est(model, *key, number_periods, est_task) 
     
-    STN['EST_ST'] = est_task
-        
-    print(production_relationship)        
-    print(number_periods)   
-    print(est_task)     
+    STN['EST_ST'] = est_task    
